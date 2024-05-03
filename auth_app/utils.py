91522d717eaa10a3f86f7f6a0c65554fe1ebb4de
Aravind R <aravind.rajeev432@gmail.com>
2024-05-03 21:38:54 +0530

    Fix: Atomic transaction for token saving

--- auth_app/utils.py
+++ auth_app/utils.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from typing import Dict, Optional
 from django.utils import timezone
 from django.conf import settings
+from django.db import transaction
 
 from auth_app.exceptions import LocationNotFound
 from .models import AuthInfo
@@ -25,58 +26,59 @@ def custom_request(
     :param data: Request body
     :return: Response from the API
     """
-    try:
-        auth = AuthInfo.objects.get(location_id=location_id)
-    except AuthInfo.DoesNotExist:
-        raise LocationNotFound  
-    current_datetime = timezone.now()
-    exp_datetime = auth.last_updated_at + timedelta(seconds=auth.expires_in)
-    if exp_datetime < current_datetime:
-        # expired
-        auth_url: str = "https://services.leadconnectorhq.com/oauth/token"
-        payload: Dict[str, str] = {
-            "client_id": settings.CLIENT_ID,
-            "client_secret": settings.CLIENT_SECRET,
-            "grant_type": "refresh_token",
-            "refresh_token": auth.refresh_token,
-        }
-        headers: Dict[str, str] = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept": "application/json",
-        }
-        response: requests.Response = requests.post(
-            auth_url, data=payload, headers=headers
-        )
-
-        auth.access_token = response.json()["access_token"]
-        auth.refresh_token = response.json()["refresh_token"]
-        auth.expires_in = response.json()["expires_in"]
-        auth.last_updated_at = timezone.now()
-        auth.save()
-
-    headers: Dict[str, str] = {
-        "Authorization": f"Bearer {auth.access_token}",
-        "Version": "2021-07-28",
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-    }
-    while True:
-        response: requests.Response = requests.request(
-            method, url, headers=headers, params=params, json=data
-        )
-        rate_limit_remaining: Optional[str] = response.headers.get(
-            "x-ratelimit-remaining", "0"
-        )
-        rate_limit_interval: Optional[str] = response.headers.get(
-            "x-ratelimit-interval-milliseconds", "10000"
-        )
+    with transaction.atomic():
         try:
-            if rate_limit_remaining and int(rate_limit_remaining) <= 3:
-                time.sleep(int(rate_limit_interval) / 1000)
-            else:
-                break
-        except Exception as e:
+            auth = AuthInfo.objects.get(location_id=location_id)
+        except AuthInfo.DoesNotExist:
+            raise LocationNotFound  
+        current_datetime = timezone.now()
+        exp_datetime = auth.last_updated_at + timedelta(seconds=auth.expires_in)
+        if exp_datetime < current_datetime:
+            # expired
+            auth_url: str = "https://services.leadconnectorhq.com/oauth/token"
+            payload: Dict[str, str] = {
+                "client_id": settings.CLIENT_ID,
+                "client_secret": settings.CLIENT_SECRET,
+                "grant_type": "refresh_token",
+                "refresh_token": auth.refresh_token,
+            }
+            headers: Dict[str, str] = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "application/json",
+            }
+            response: requests.Response = requests.post(
+                auth_url, data=payload, headers=headers
+            )
 
-            raise (e)
+            auth.access_token = response.json()["access_token"]
+            auth.refresh_token = response.json()["refresh_token"]
+            auth.expires_in = response.json()["expires_in"]
+            auth.last_updated_at = timezone.now()
+            auth.save()
 
-    return response
+        headers: Dict[str, str] = {
+            "Authorization": f"Bearer {auth.access_token}",
+            "Version": "2021-07-28",
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+        while True:
+            response: requests.Response = requests.request(
+                method, url, headers=headers, params=params, json=data
+            )
+            rate_limit_remaining: Optional[str] = response.headers.get(
+                "x-ratelimit-remaining", "0"
+            )
+            rate_limit_interval: Optional[str] = response.headers.get(
+                "x-ratelimit-interval-milliseconds", "10000"
+            )
+            try:
+                if rate_limit_remaining and int(rate_limit_remaining) <= 3:
+                    time.sleep(int(rate_limit_interval) / 1000)
+                else:
+                    break
+            except Exception as e:
+
+                raise (e)
+
+        return response
